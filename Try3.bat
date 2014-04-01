9cf21cee3bb1a3e8e18bae00c66263beb054e82f
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 18:12:34 -0700

    Update Try3.bat
    test3

--- Try3.bat
+++ Try3.bat
@@ -5,8 +5,9 @@ FOR /D /r %%F in ("*") DO (
     pushd %CD%
     cd %%F
     FOR %%X in (*.exe *.dll) DO (
-        set current_file=%%F
-        echo ##teamcity[testStarted name='%current_file%']
+        set current_file=%%X
+        echo %current_file% 
+        echo ##teamcity[testStarted name="%current_file%"]
         FOR /f "delims=" %%i in ('"D:\Tools\SignTool\signtool.exe" verify /pa %%X 2^>^&1') do set output=%%i
 	If NOT "%output%"=="%output:Successfully verified=%" (
             echo ##teamcity[name='%current_file%' message='%output%']
