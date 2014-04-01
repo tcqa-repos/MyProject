4317653ffc7ca28c64acc1279425c9a7fb9ac9e8
Masha <julia.o.reshetnikova@gmail.com>
2014-04-01 00:44:51 -0700

    Update Fetch.bat
    ghj

--- Fetch.bat
+++ Fetch.bat
@@ -1,3 +1,3 @@
 @echo on
-FOR /f "delims=" %%i in ('"D:\Tools\curl.exe" GET https://%username%:%password%@buildserver/httpAuth/app/rest/artifacts?locator=id:TC_Trunk_BuildDist' ) set output = %%i
+FOR /f "delims=" %%i in ('"D:\Tools\curl.exe" GET https://%username%:%password%@buildserver/httpAuth/app/rest/artifacts?locator=id:TC_Trunk_BuildDist' ) do set output = %%i
 echo %output%
