bce264fef708b5355441089417a2e260417fcf17
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 20:17:29 -0700

    Update Try3.bat
    try formatting

--- Try3.bat
+++ Try3.bat
@@ -1,4 +1,11 @@
 @echo on
 @setlocal enabledelayedexpansion
-FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.*) DO ( set file=%%X & echo !file!  
-  IF NOT [%file%]==[] echo ##teamcity[testStarted name='%file%'] & echo ##teamcity[testFinished name='%file%'] ) & popd )
+FOR /D %%F in ("*") DO (
+    pushd %CD% & cd %%F
+    FOR %%X in (*.*) DO (
+        set file=%%X
+        echo !file!  
+        IF NOT [%file%]==[] 
+            echo ##teamcity[testStarted name='%file%']
+            echo ##teamcity[testFinished name='%file%'] )
+popd )
