13338a56b1c0af34c88cdb26cf126d6461da16e2
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 20:35:22 -0700

    Update Try3.bat
    this won't work

--- Try3.bat
+++ Try3.bat
@@ -3,9 +3,8 @@
 @FOR /D %%F in ("*") DO (
     @pushd %CD% & cd %%F
     @FOR %%X in (*.*) DO (
-        @set file="%%X"
-        @echo !file!  
-        @IF NOT [!file!]==[] (
-            @echo ##teamcity[testStarted name='!file!']
-            @echo ##teamcity[testFinished name='!file!'] ) )
+        @set current_file=%%F\%%X
+        @IF NOT [!current_file!]==[] (
+            @echo ##teamcity[testStarted name='!current_file!']
+            @echo ##teamcity[testFinished name='!current_file!'] ) )
 @popd )
