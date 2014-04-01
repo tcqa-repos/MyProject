971e7de1bfbdb3fa14eb729e6785857689be7458
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 19:35:08 -0700

    Update Try3.bat
    and again

--- Try3.bat
+++ Try3.bat
@@ -1,3 +1,3 @@
 @echo on
-FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.*) DO ( set current_file=%%X & echo %%X 
-  IF NOT [%current_file%]==[] echo ##teamcity[testStarted name='%current_file%'] & echo ##teamcity[testFinished name='%current_file%'] ) & popd )
+FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.*) DO ( set file=%%X & echo %file%  
+  IF NOT [%file%]==[] echo ##teamcity[testStarted name='%file%'] & echo ##teamcity[testFinished name='%file%'] ) & popd )
