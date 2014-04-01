b2c0d049fe47ec3dc278d258c376fdf4c5ba1dd3
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 21:00:32 -0700

    Update Try3.bat
    should work

--- Try3.bat
+++ Try3.bat
@@ -1,10 +1,11 @@
 @echo off
 @setlocal enabledelayedexpansion
+@set init_path=%CD%
 @FOR /D /r %%F in ("*") DO (
     @pushd %CD% & cd %%F
     @FOR %%X in (*.exe *.dll) DO (
         @set file_path=%%F\%%X
-        @set current_file=!file_path:*%~dp0%\=!
+        @set current_file=!file_path:%init_path%\=!
         @IF NOT [!current_file!]==[] (
             @echo ##teamcity[testStarted name='!current_file!']
             @FOR /f "delims=" %%i in ('"D:\Tools\SignTool\signtool.exe" verify /pa %%X 2^>^&1') do set output=%%i
