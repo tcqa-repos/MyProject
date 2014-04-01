109bbcae5d3ebecd5e0222a3cdab642df3376ac9
Masha <julia.o.reshetnikova@gmail.com>
2014-04-01 00:59:38 -0700

    Update Fetch.bat
    hjk

--- Fetch.bat
+++ Fetch.bat
@@ -1,3 +1,3 @@
 @echo on
-FOR /f "delims=" %%i in ('"D:\Tools\curl.exe" -v --basic --user %username%:%password% --request GET https://buildserver.labs.intellij.net/httpAuth/app/rest/artifacts?locator=id:TC_Trunk_BuildDist' ) do set output = %%i
+FOR /f "delims=" %%i in ('"D:\Tools\curl.exe" -v --basic --user %username%:%password% --request GET https://buildserver.labs.intellij.net/httpAuth/app/rest/artifacts' ) do set output = %%i
 echo %output%
