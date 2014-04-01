e9bc0d21cf19d6baa67a073bdd31af478b562179
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 19:05:36 -0700

    Update Try3.bat
    fix

--- Try3.bat
+++ Try3.bat
@@ -1,3 +1,3 @@
 @echo on
-FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.exe *.dll) DO ( set current_file=%%F\&&X & echo %current_file% 
-        echo "##teamcity[testStarted name='%current_file%']" ) & popd )
+FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.exe *.dll) DO ( set current_file=%%F\%%X & echo %current_file% 
+        echo ##teamcity[testStarted name='%current_file%'] ) & popd )
