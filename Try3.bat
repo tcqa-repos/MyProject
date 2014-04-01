9b5c260c69f0d300e5d36c162a8fbf361a3a1e63
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 20:48:10 -0700

    Update Try3.bat
    looks fine...

--- Try3.bat
+++ Try3.bat
@@ -7,10 +7,13 @@
         @IF NOT [!current_file!]==[] (
             @echo ##teamcity[testStarted name='!current_file!']
             @FOR /f "delims=" %%i in ('"D:\Tools\SignTool\signtool.exe" verify /pa %%X 2^>^&1') do set output=%%i
-	        @If NOT "!output!"=="!output:Successfully verified=!" (
+	        @IF NOT "!output!"=="!output:Successfully verified=!" (
                     @echo ##teamcity[name='!current_file!' message='!output!']
-	        @) else (
+	        ) else (
                     @echo ##teamcity[testFailed name='!current_file!' message='!output!']
-                @)
-            @echo ##teamcity[testFinished name='!current_file!'] ) )
-@popd )
+                )
+            @echo ##teamcity[testFinished name='!current_file!']
+        ) 
+    )
+    @popd 
+)
