a558aae8bb34d48a18e65891ae2ad27ff5dd1523
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 19:53:45 -0700

    Update Try3.bat
    and again

--- Try3.bat
+++ Try3.bat
@@ -1,4 +1,4 @@
 @echo on
-setlocal enabledelayedexpansion
+@setlocal enabledelayedexpansion
 FOR /D %%F in ("*") DO (pushd %CD% & cd %%F & FOR %%X in (*.*) DO ( set file=%%X & echo !file!  
   IF NOT [%file%]==[] echo ##teamcity[testStarted name='%file%'] & echo ##teamcity[testFinished name='%file%'] ) & popd )
