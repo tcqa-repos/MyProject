ff66d855bb91a297c34220ac61a268f53bae735e
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 20:20:45 -0700

    Update Try3.bat
    try dogging

--- Try3.bat
+++ Try3.bat
@@ -1,11 +1,11 @@
 @echo on
 @setlocal enabledelayedexpansion
-FOR /D %%F in ("*") DO (
-    pushd %CD% & cd %%F
-    FOR %%X in (*.*) DO (
-        set file=%%X
-        echo !file!  
-        IF NOT [%file%]==[] DO (
-            echo ##teamcity[testStarted name='%file%']
-            echo ##teamcity[testFinished name='%file%'] ) )
-popd )
+@FOR /D %%F in ("*") DO (
+    @pushd %CD% & cd %%F
+    @FOR %%X in (*.*) DO (
+        @set file=%%X
+        @echo !file!  
+        @IF NOT [%file%]==[] DO (
+            @echo ##teamcity[testStarted name='%file%']
+            @echo ##teamcity[testFinished name='%file%'] ) )
+@popd )
