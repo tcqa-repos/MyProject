5b115dd6cdf7582004038123c119b559c0d9ad6c
Masha <julia.o.reshetnikova@gmail.com>
2014-03-31 18:46:25 -0700

    Update Try3.bat
    trying F again

--- Try3.bat
+++ Try3.bat
@@ -2,7 +2,7 @@
 
 REM signature verification
 FOR /D /r %%F in ("*") DO (pushd %CD% & cd %%F
-    FOR %%X in (*.exe *.dll) DO ( set current_file=%%X & echo %current_file% 
+    FOR %%X in (*.exe *.dll) DO ( set current_file=%%F & echo %current_file% 
         echo "##teamcity[testStarted name='%current_file%']"
         FOR /f "delims=" %%i in ('"D:\Tools\SignTool\signtool.exe" verify /pa %%X 2^>^&1') do set output=%%i
 	If NOT "%output%"=="%output:Successfully verified=%" (
